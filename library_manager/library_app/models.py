58566c9dd356a35370950c5199cb953f81e4e737
N0935198 <N0935198@ntu.ac.uk>
2024-08-22 05:17:53 +0100

    update: each member can see their borrowed books and when they are due and models have been adjusted to allow null fields

--- library_manager/library_app/models.py
+++ library_manager/library_app/models.py
@@ -1,10 +1,17 @@
 from django.db import models
-
 # Create your models here.
+
+
 class Member(models.Model):
+    roles = (
+        ('admin', 'Admin'),
+        ('member', 'Member')
+    )
+
     member_username = models.CharField(max_length=32, unique=True)
     member_first_name = models.CharField(max_length=32)
     member_last_name = models.CharField(max_length=32)
+    member_role = models.CharField(choices=roles, blank=True, null=True)
 
     def __str__(self):
         return f"{self.member_first_name} {self.member_last_name}"
@@ -14,8 +21,8 @@ class Library(models.Model):
     book_author = models.CharField(max_length=64)
     book_year = models.IntegerField()
     book_borrowed = models.BooleanField(default=False)
-    book_holder = models.ForeignKey(Member, on_delete=models.CASCADE)
-    book_return_date = models.DateField()
+    book_holder = models.ForeignKey(Member, on_delete=models.CASCADE, blank=True, null=True)
+    book_return_date = models.DateField(blank=True, null=True)
 
     def __str__(self):
         return f"{self.book_name} by {self.book_author} ({self.book_year})"
--- library_manager/library_app/views.py
+++ library_manager/library_app/views.py
@@ -1,7 +1,7 @@
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate
-
+from .models import Library, Member
 
 # Create your views here.
 
@@ -14,18 +14,33 @@ def login(request):
             return HttpResponseRedirect('/library/')
         else:
             request.session['login'] = False
-            return HttpResponse("Incorrect Credentials: 401")
+            return render(request, 'login.html', {
+                'error_message': 'Incorrect Credentials'
+            })
     return render(request, 'login.html')
 
-
 def library(request):
+    queryset = Library.objects.all()
     if request.session.get('login', True):
-        return render(request, 'library.html')
+        return render(request, 'library.html', {
+            'books': queryset,
+            'borrowedBooks': getBorrowedBooks(request.session.get('username'))
+        })
+
+def getBorrowedBooks(username):
+    try:
+        member = Member.objects.get(member_username=username)
+        if member:
+            borrowedBooks = Library.objects.filter(book_holder=member)
+            return borrowedBooks
+    except:
+        return Library.objects.none()
 
 
 def member(request):
+    queryset = Member.objects.all()
     if request.session.get('login', True):
-        return render(request, 'member.html')
+        return render(request, 'member.html', { 'members': queryset })
 
 def logout(request):
     request.session['login'] = True
