698ca1c78b0f8f3280b3f6873370e61583a15b39
N0935198 <N0935198@ntu.ac.uk>
2024-08-31 17:58:44 +0100

    update: added borrowing of books from library and adjusted the layout

--- library_manager/library_app/urls.py
+++ library_manager/library_app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 from django.contrib.auth.views import LogoutView
+
+from . import views
 from .views import login, library, member
 
 
@@ -8,5 +10,6 @@ urlpatterns = [
     path('login/', login, name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('library/', library, name='library'),
+    path('library/borrow/', views.borrowBook, name='borrow'),
     path('member/', member, name='member'),
 ]
--- library_manager/library_app/views.py
+++ library_manager/library_app/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
-from django.shortcuts import render
+from datetime import datetime, timedelta
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate
 from .models import Library, Member
 
@@ -23,8 +24,9 @@ def library(request):
     queryset = Library.objects.all()
     if request.session.get('login', True):
         return render(request, 'library.html', {
-            'books': queryset,
-            'borrowedBooks': getBorrowedBooks(request.session.get('username'))
+            'booksInLibrary': queryset.filter(book_borrowed=False),
+            'bookswithMembers': queryset.filter(book_borrowed=True),
+            'userborrowedBooks': getBorrowedBooks(request.session.get('username'))
         })
 
 def getBorrowedBooks(username):
@@ -36,6 +38,16 @@ def getBorrowedBooks(username):
     except:
         return Library.objects.none()
 
+def borrowBook(request):
+    if request.session.get('login', True):
+        book_name = request.GET.get('bookname')
+        book = Library.objects.get(book_name=book_name)
+        book.book_borrowed = True
+        member = Member.objects.get(member_username=request.session.get('username'))
+        book.book_holder = member
+        book.book_return_date = datetime.now() + timedelta(days=14)
+        book.save()
+        return redirect(library)
 
 def member(request):
     queryset = Member.objects.all()
